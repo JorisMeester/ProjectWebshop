96a0d3cbff970d4dcbe001bdf76f9db9daa2e737
JorisMeester <jorismeester98@gmail.com>
2017-12-21 11:20:10 +0100

    seed admin

--- ProjectWebshop/Migrations/Configuration.cs
+++ ProjectWebshop/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace ProjectWebshop.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using ProjectWebshop.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,6 +21,23 @@ namespace ProjectWebshop.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "admin@admin.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "admin@admin.com" };
+                manager.Create(user, "Password123!");
+                manager.AddToRole(user.Id, "Admin");
+            }
         }
     }
 }
